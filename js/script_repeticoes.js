f7f3addfb692d7e9376c5b6d3aab2879f3a83f4d
Ana Rute Da Silva Santos <ana.silvasantos@aluno.senai.br>
2026-06-16 16:58:41 -0300

    atualizado a estrutura de repetiçaõ

--- js/script_repeticoes.js
+++ js/script_repeticoes.js
@@ -1,4 +1,28 @@
 //ESTRUTURA DE REPETIÇÃO FOR
-for(i = 0;i < 10; i++){
-console.log(i)
+const divFor = document.querySelector('#div-for')
+
+for(i = 0;i < 10; i++ ){
+   console.log(i,'Dinheiro')
+   divFor.innerHTML += `${i} - Dinheiro <br>`
+
 }
+
+//CONTADOR/ACUMULADOR
+const inputNum = document.querySelector('#num')
+const btnNum = document.querySelector('#btn-num')
+const divContAcum = document.querySelector('#div-cont-acum')
+
+let cont = 0
+let acum = 0.0
+
+btnNum.addEventListener('click', (evt) =>{
+    let numDigitado = Number(inputNum.value)
+ 
+    cont ++
+    acum += numDigitado
+
+ divContAcum.innerHTML = `TOTAL DE NÚMERO DIITADO É: ${cont} <br>
+ A SOMA DOS NÚMEROS É: ${acum}`
+
+ inputNum.value = ''
+} )
